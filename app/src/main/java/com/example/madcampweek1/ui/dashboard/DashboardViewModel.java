8cfbbeb1e65a37d8e53880d8842244214ddaee7d
hw3090 <hw3090@gmail.com>
2023-07-03 13:52:54 +0900

    0703 ìbefore scrum

--- app/src/main/java/com/example/madcampweek1/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/madcampweek1/ui/dashboard/DashboardViewModel.java
@@ -7,6 +7,11 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DashboardViewModel extends ViewModel {
 
 
@@ -16,11 +21,22 @@ public class DashboardViewModel extends ViewModel {
 
         numLines = new MutableLiveData<>();
         numLines.setValue("Number of Lines : 0");
-
+        dataListLiveData.setValue(new ArrayList<>());
     }
 
     public LiveData<String> getNumLines(){return numLines;}
+    private final MutableLiveData<List<Map<String, String>>> dataListLiveData = new MutableLiveData<>();
 
+    public void setDataList(List<Map<String, String>> dataList) {
+        dataListLiveData.setValue(dataList);
+    }
+
+    public List<Map<String, String>> getDataList() {
+        return dataListLiveData.getValue();
+    }
+    public LiveData<List<Map<String, String>>> getDataListLive() {
+        return dataListLiveData;
+    }
     public int getNumLinesInt(){
         String[] splitArray = numLines.getValue().split(" ") ;
         return Integer.parseInt(splitArray[splitArray.length-1]);
