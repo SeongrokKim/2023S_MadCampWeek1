4d2034d42b8fd98b820121dcc06dea67dcba8f65
hw3090 <hw3090@gmail.com>
2023-07-04 10:15:26 +0900

    Revert "Timer added"
    This reverts commit ffda447be0125e1d46d5712eb56dbaf8f4f66ea2.

--- app/src/main/java/com/example/madcampweek1/MainActivity.java
+++ app/src/main/java/com/example/madcampweek1/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.madcampweek1;
 
 import android.os.Bundle;
-import android.widget.PopupWindow;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,7 +15,7 @@ import com.example.madcampweek1.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private PopupWindow popupWindow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,10 +31,5 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-    public PopupWindow getPopupWindow(){
-        return popupWindow;
-    }
-    public void setPopupWindow(PopupWindow popupWindow){
-        this.popupWindow = popupWindow;
-    }
+
 }
--- app/src/main/java/com/example/madcampweek1/ui/notifications/TimerFragment.java
+++ app/src/main/java/com/example/madcampweek1/ui/notifications/TimerFragment.java
@@ -1,155 +1,67 @@
 package com.example.madcampweek1.ui.notifications;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.madcampweek1.MainActivity;
+import com.example.madcampweek1.databinding.FragmentImageAugmentBinding;
 import com.example.madcampweek1.databinding.FragmentTimerBinding;
-import com.google.android.material.button.MaterialButtonToggleGroup;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.example.madcampweek1.ui.dashboard.ImageAugmentFragment;
 
 public class TimerFragment extends Fragment {
 
-    private static final String TAG = "Timer Fragment";
+    private int imageResourceId;
     private FragmentTimerBinding binding;
-    private TextView textViewName;
-    private TextView textViewExercise;
-    private TextView textViewReps;
-    private TextView textViewTimer;
-    private Button buttonSkip;
-    private Button buttonStart;
-    private Button buttonStop;
-    private Button buttonExit;
-    private int exerciseIndex = -1;
 
-    private int reps = -1;
-    private String routineName = "Err";
-    private List<Map<String, String>> exercises = new ArrayList<>();
-
+    public static ImageAugmentFragment newInstance(String imagePath) {
+        ImageAugmentFragment fragment = new ImageAugmentFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_IMAGE_RESOURCE_ID, imagePath);
+        fragment.setArguments(args);
+        return fragment;
+    }
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Toast.makeText(getContext(), "OnCreate", Toast.LENGTH_SHORT).show();
-        //Toast.makeText(getContext(), getArguments().toString(), Toast.LENGTH_SHORT).show();
-
+        Toast.makeText(getContext(), "first", Toast.LENGTH_SHORT).show();
+        if (getArguments() != null) {
+            imageResourceId = getArguments().getInt(ARG_IMAGE_RESOURCE_ID);
+        }
     }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        Toast.makeText(getContext(), "OncreateView", Toast.LENGTH_SHORT).show();
-
+        // ImageAugmentFragment의 레이아웃을 inflate하고 반환합니다.
         binding = FragmentTimerBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        textViewName = binding.textViewName;
-        textViewExercise = binding.textViewExercise;
-        textViewTimer = binding.textViewTimer;
-
-        textViewReps = binding.textViewReps;
-        buttonExit = binding.buttonExit;
-        buttonSkip = binding.buttonSkip;
-        buttonStart = binding.buttonStart;
-        buttonStop = binding.buttonStop;
-
-
-        binding.buttonExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // PopupWindow를 닫기
-                if (getActivity() != null) {
-                    PopupWindow popupWindow = ((MainActivity) getActivity()).getPopupWindow();
-                    if (popupWindow != null) {
-                        popupWindow.dismiss();
-                    }
-                }
-
-                // 원래 Fragment로 돌아가기
-                FragmentManager fragmentManager = getParentFragmentManager();
-                fragmentManager.popBackStack();
-            }
-        });
-        Toast.makeText(getContext(), "Onclick1", Toast.LENGTH_SHORT).show();
-
-        binding.buttonSkip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Skip 버튼 클릭 이벤트 처리
-                // ...
-            }
-        });
-
-        binding.buttonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start 버튼 클릭 이벤트 처리
-                // ...
-            }
-        });
-
-        binding.buttonStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Stop 버튼 클릭 이벤트 처리
-                // ...
-            }
-        });
-        Toast.makeText(getContext(), "Onclick2", Toast.LENGTH_SHORT).show();
-
-
-        // 데이터 추출
-        Bundle arguments = getArguments();
-        if (true || arguments != null) {
-//            // argument로 인자 넣어주기
-//            routineName = arguments.getString("routineName");
-//            reps = arguments.getInt("reps");
-//            exercises = (List<Map<String, String>>) arguments.getSerializable("exercises");
-
-            //임시 값
-            routineName = "Tmp Rt";
-            reps = 3;
-            exercises = new ArrayList<>();
-            Map<String, String> exMap = new HashMap<>();
-            exMap.put("name", "TmpEx");
-            exMap.put("time", "00:12:34");
-            exercises.add(exMap);
-            exMap.put("name", "TmpRest");
-            exMap.put("time", "00:00:10");
-            exercises.add(exMap);
-
-            // 데이터 활용
-            textViewName.setText(routineName);
-            textViewExercise.setText(exercises.get(0).get("name"));
-            textViewTimer.setText(exercises.get(0).get("time"));
-            textViewReps.setText(Integer.toString(reps));
-            exerciseIndex = 0;
+        ImageView imageView = binding.imageView;
+        if (getArguments() != null) {
+            imageResourceId = getArguments().getInt(ARG_IMAGE_RESOURCE_ID);
         }
-        else{
-            Toast.makeText(getContext(), "헤헤 죠졌네 이거", Toast.LENGTH_SHORT).show();
-        }
+
+        // imageResourceId를 사용하여 이미지를 로드하고 ImageView에 설정하는 로직을 구현
+        imageView.setImageResource(imageResourceId);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 프래그먼트를 종료하여 이전 화면으로 돌아감
+                requireActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
 
         return root;
+
     }
-
-
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
