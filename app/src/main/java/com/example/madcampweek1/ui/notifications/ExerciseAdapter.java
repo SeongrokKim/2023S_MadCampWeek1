fd172d7abe24debd93abbe225da849dee6fab18f
SeongrokKim <tjdfhr1228@gmail.com>
2023-07-04 23:43:45 +0900

    add delete button

--- app/src/main/java/com/example/madcampweek1/ui/notifications/ExerciseAdapter.java
+++ app/src/main/java/com/example/madcampweek1/ui/notifications/ExerciseAdapter.java
@@ -3,12 +3,14 @@ package com.example.madcampweek1.ui.notifications;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.madcampweek1.R;
+import com.google.android.material.badge.BadgeUtils;
 
 import java.util.ArrayList;
 
@@ -30,6 +32,15 @@ public class ExerciseAdapter extends RecyclerView.Adapter<ExerciseAdapter.Exerci
         ExerciseItem exerciseItem = exerciseItemArrayList.get(position);
 
         holder.setExerciseItem(exerciseItem);
+
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exerciseItemArrayList.remove(position);
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position, exerciseItemArrayList.size());
+            }
+        });
     }
 
     @Override
@@ -42,12 +53,14 @@ public class ExerciseAdapter extends RecyclerView.Adapter<ExerciseAdapter.Exerci
         private EditText timeExerciseCho;
         private EditText timeExerciseBun;
         private ExerciseItem exerciseItem;
+        private Button btnDelete;
 
         public ExerciseViewHolder(@NonNull View itemView) {
             super(itemView);
             nameExercise = itemView.findViewById(R.id.nameExercise);
             timeExerciseCho = itemView.findViewById(R.id.timeExerciseBun);
             timeExerciseBun = itemView.findViewById(R.id.timeExerciseCho);
+            btnDelete = itemView.findViewById(R.id.btnDelete);
         }
         public ExerciseItem getExerciseItem(){
             return exerciseItem;
